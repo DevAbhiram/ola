02fbd50844be04c3bb0207584462187245a653ba
Abhiram <161499134+DevAbhiram@users.noreply.github.com>
2025-02-11 07:44:26 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,2 +1,9 @@
 print("Hello")
 print("1")
+def factorial_recursive(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial_recursive(n - 1)
+
+num = int(input("Enter a number: "))
+print("Factorial:", factorial_recursive(num))
