b016b88b0e2e75b7d3a862feade7e3f45df813af
Abhiram <161499134+DevAbhiram@users.noreply.github.com>
2025-02-11 07:45:36 +0530

    Update python.py

--- python.py
+++ python.py
@@ -7,3 +7,51 @@ def factorial_recursive(n):
 
 num = int(input("Enter a number: "))
 print("Factorial:", factorial_recursive(num))
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("None")
+
+    def delete(self, key):
+        temp = self.head
+        if temp and temp.data == key:
+            self.head = temp.next
+            return
+        prev = None
+        while temp and temp.data != key:
+            prev = temp
+            temp = temp.next
+        if temp:
+            prev.next = temp.next
+
+# Usage
+ll = LinkedList()
+ll.insert(10)
+ll.insert(20)
+ll.insert(30)
+ll.display()  # Output: 10 -> 20 -> 30 -> None
+ll.delete(20)
+ll.display()  # Output: 10 -> 30 -> None
+
